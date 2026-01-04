3fdbeba01db0be5a06397cff1d98631fff51515b
EarlSpiritRoo <191369394+EarlSpiritRoo@users.noreply.github.com>
2026-01-04 23:37:50 +0700

    Fix mobile hero layout and add FlipWords component

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,21 +1,57 @@
+import { FlipWords } from "./FlipWords";
+import { motion } from "motion/react";
+
 const HeroText = () => {
+  const words=["Secure", "Modern", "Scalable"];
+  const variants ={
+    hidden: {opacity: 0, x: -50},
+    visible: {opacity: 1, x: 0 },
+  };
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/* Desktop View */}
         <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">Hi I'm Yasmine</h1>
+            <motion.h1 
+            className="text-4xl font-medium" 
+            initial={{opacity: 0, x: -50 }}
+            animate={{opacity: 1, x: 0 }}
+            transition={{delay: 1 }}
+            >
+              Hi I'm Yasmine
+              </motion.h1>
             <div className="flex flex-col items-start">
-                <p className="text-5xl font-medium text-neutral-300">A Developer <br /> Dedicated to Crafting
-                </p>
-                {/* <div>get flip word from aceternity</div> 
-                */}
-                <p className="text-4xl font-medium text-neutral-300"> Web Solutions
-                </p>
+                <motion.p className="text-5xl font-medium text-neutral-300"
+                initial={{opacity: 0, x: -50 }}
+                  animate={{opacity: 1, x: 0 }}
+                  transition={{delay: 1.2 }}
+                >
+                  A Developer <br /> Dedicated to Crafting
+                </motion.p>
+                <motion.div>
+                    <FlipWords 
+                    words={words} 
+                    className="font-black text-white text-8xl"
+                    />
+                </motion.div>
+                <motion.p className="text-4xl font-medium text-neutral-300"> Web Solutions
+                </motion.p>
             </div>
         </div>
         {/* Mobile View */}
+        <div className="flex flex-col space-y-6 md:hidden">
+          <motion.p className="text-4xl font-medium">Hi, I am Yasmine</motion.p>
+          <div>
+            <motion.p className="text-5xl font-black text-neutral-300">Builing</motion.p>
+            <motion.div>
+              <FlipWords 
+                words={words} 
+                className="font-bold text-white text-7xl"/>
+            </motion.div>
+            <motion.p className="text-4xl font-black text-neutral-300">Web Apllications</motion.p>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default HeroText
